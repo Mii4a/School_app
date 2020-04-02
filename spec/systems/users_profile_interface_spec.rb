d1809465c479cc6fbf607239eccd45c4f63fad6e
Mii4a <syoutacancer@gmail.com>
2020-04-02 06:47:16 +0000

    fix school profile test

--- spec/systems/users_profile_interface_spec.rb
+++ spec/systems/users_profile_interface_spec.rb
@@ -5,13 +5,14 @@ RSpec.describe "UsersProfile", type: :system do
 let(:user){ create(:user) }
 let(:schools){ create_list(:school, 10) }
 
-describe "Profile" do
-  it "checks each items" do
-    visit user_path(user)
-    expect(page).to have_content user.name
-    expect(page).to have_css "img.gravatar"
-    expect(page).to have_content user.schools.count.to_s
+  describe "Profile" do
+    it "checks each items" do
+      system_login_as(user)
+      visit user_path(user)
+      expect(page).to have_content user.name
+      expect(page).to have_css "img.gravatar"
+      expect(page).to have_content user.schools.count.to_s
+    end
   end
-end
 
 end
