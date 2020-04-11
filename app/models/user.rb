3dd33d6599583419c7647477e00c6c18ec602f9c
Mii4a <syoutacancer@gmail.com>
2020-04-11 18:15:54 +0000

    Fix feed

--- app/models/user.rb
+++ app/models/user.rb
@@ -83,7 +83,8 @@ class User < ApplicationRecord
     end
     
     def feed
-        School.where("user_id=?", id)
+        following_ids = "SELECT followed_id FROM user_relationships WHERE follower_id = :user_id"
+        School.where("user_id IN (#{following_ids}) OR user_id = :user_id", user_id: id)
     end
     
     def enter(school)
