b811852173978187effbc51afc6eadd32a256663
Mii4a <syoutacancer@gmail.com>
2020-04-14 08:25:21 +0000

    Add following and follower page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,6 +3,7 @@ class StaticPagesController < ApplicationController
   def home
     if logged_in?
       @user = current_user
+      @school = current_user.feed.page(params[:page]).per(10)
       @school_build = current_user.schools.build
       @feed_items = current_user.feed.page(params[:page]).per(10)
     end
